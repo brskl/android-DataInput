1500d00aa9149c71ecd7891b828b49928cf25499
Benjamin <brskl@msn.com>
2016-11-13 22:31:19 -0800

    Added code to enable/disable OK button

--- app/src/main/java/com/example/ben/datainput/MainActivity.java
+++ app/src/main/java/com/example/ben/datainput/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ben.datainput;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickToggle(View view) {
+
+        Switch sw = (Switch) view;
+
+        View btn = this.findViewById(R.id.uxBtnOk);
+        btn.setEnabled(sw.isChecked());
+
     }
 }
